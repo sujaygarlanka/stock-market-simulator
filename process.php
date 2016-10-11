9a6ca1d8e1a9d43dc1b57e259299dddff1a216de
Sujay Garlanka <sujay.garlanka@gmail.com>
2016-10-11 15:35:04 -0400

    process.php edited

--- process.php
+++ process.php
@@ -14,12 +14,17 @@ $selected_ticker = $_POST['stock']; // ticker or stock selected from the table
 while($row = mysqli_fetch_array($query_info)){
     $ticker=$row['stock'];
     if (strcmp($ticker,'account')!=0){
-        $response = file_get_contents("http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22$ticker%22)&env=http://datatables.org/alltables.env&format=json");
+        $response = @file_get_contents("http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22$ticker%22)&env=http://datatables.org/alltables.env&format=json");
+        if ($response == false){
+            continue;
+        }
         $response = json_decode($response,true); // the true returns an associative array from json
         $name = $response['query']['results']['quote']['Name'];
         $price = $response['query']['results']['quote']['Ask'];
         $float_price = (float)$price;
-        updateRow($ticker,-1,-1,$float_price);
+        if ($float_price != 0){
+            updateRow($ticker,-1,-1,$float_price);
+        }
     }
     if (strcmp($ticker, $selected_ticker)==0){
         $stock = readRow("$selected_ticker"); // stock selected to be read and returned to display
